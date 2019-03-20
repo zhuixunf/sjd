9d306e787d6251e8857491176a113dfe087131df
peng727336957 <727336957@qq.com>
2019-03-20 20:45:56 +0800

    1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+def post_update(request, id):
+    post = Post.objects.get(id=id)
+    if request.method == 'POST':
+        post_form = PostForm(data=request.POST, instance=post)
+        post_form.save()
+        return redirect('/')
+    else:
+        post_form = PostForm(instance=post)
+    return render(request, 'post_update.html', {'post_form': post_form})
